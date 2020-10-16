9ee00e877d3e0b54e618ffb07e0fc250f6c20c1a
gaofeng1018 <775927889@qq.com>
2020-10-16 15:07:46 +0800

    1015作业更新

--- testing1/test_calc.py
+++ testing1/test_calc.py
@@ -9,18 +9,17 @@ class TestCalc:
 
     def teardown_class(self):
         print("计算结束")
+
     #加法测试
-    @pytest.mark.parametrize('a,b,expect',[
-        [1, 1, 2], [100, 100, 200], [0.1, 0.3, 0.4], [-1, -1, -2], [1, 0, 1]
+    @pytest.mark.parametrize('a,b,expect', [
+        [1, 1, 2], [1000, 10000, 11000], [0.1, 0.3, 0.4], [-1, -1, -2], [0, 0, 0]
     ], ids = ['int_case', 'bignum_case', 'float_case', 'minus_case', 'zero_case'])
     def test_add(self, a, b, expect):
-        # calc = Calculator()
         result = self.calc.add(a, b)
         assert result == expect
 
     #数字+字符串
     def test_add1(self):
-        # calc = Calculator()
         try:
             result = self.calc.add(1, '1')
         except TypeError as e:
@@ -32,12 +31,10 @@ class TestCalc:
         [10, 3, 10 / 3], [10, 2, 5], [100000, 2, 50000]
     ])
     def test_div(self, a, b, expect):
-        # calc = Calculator()
         result = self.calc.div(a, b)
         assert result == expect
     #除数为0
     def test_div1(self):
-        # calc = Calculator()
         try:
             result = self.calc.div(1, 0)
         except ZeroDivisionError as e:
