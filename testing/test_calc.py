22e2779708a76aba0c09fa36ca342286819d8a7d
gaofeng1018 <775927889@qq.com>
2020-10-22 11:56:07 +0800

    第二次作业更新

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -5,50 +5,21 @@ import pytest
 import yaml
 import allure
 
-
-def get_datas_add():
+def get_datas():
     with open("./datas/calc.yml") as f:
         datas = yaml.safe_load(f)
     add_datas = datas["add"]["datas"]
     add_ids = datas["add"]["ids"]
     print(add_datas)
     print(add_ids)
-    return [add_datas, add_ids]
-
-
-def get_datas_div():
-    with open("./datas/calc.yml") as g:
-        datas_div = yaml.safe_load(g)
-    div_data = datas_div["div"]["datas"]
-    print(div_data)
-    return [div_data]
-
-
-def get_datas_sub():
-    with open("./datas/calc.yml") as f:
-        datas_sub = yaml.safe_load(f)
-    sub_data = datas_sub["sub"]["datas"]
-    sub_ids = datas_sub["sub"]["ids"]
-    print(sub_data)
-    print(sub_ids)
-    return [sub_data, sub_ids]
-
-
-def get_datas_mul():
-    with open("./datas/calc.yml") as f:
-        datas_mul = yaml.safe_load(f)
-    mul_data = datas_mul["mul"]["datas"]
-    mul_ids = datas_mul['mul']['ids']
-    print(mul_data)
-    return [mul_data, mul_ids]
-
+    return datas
 
 @allure.feature("测试模块")
 class TestCalc:
     # 加法测试
     # @allure.story("加法测试")
     @pytest.mark.run(order=1)
-    @pytest.mark.parametrize('a,b,expect', get_datas_add()[0], ids=get_datas_add()[1])
+    @pytest.mark.parametrize('a,b,expect', get_datas()["add"]["datas"], ids=get_datas()["add"]["ids"])
     def test_add(self, get_calc, a, b, expect):
         # result = self.calc.add(a, b)
         result = get_calc.add(a, b)
@@ -67,7 +38,7 @@ class TestCalc:
     # 减法测试
     # @allure.story("减法测试")
     @pytest.mark.run(order=7)
-    @pytest.mark.parametrize('a,b,expect', get_datas_sub()[0], ids=get_datas_sub()[1])
+    @pytest.mark.parametrize('a,b,expect', get_datas()["sub"]["datas"], ids=get_datas()["sub"]["ids"])
     def test_sub(self, get_calc, a, b, expect):
         # result = self.calc.add(a, b)
         result = get_calc.sub(a, b)
@@ -86,7 +57,7 @@ class TestCalc:
     # 除法测试
     # @allure.story("除法测试")
     @pytest.mark.run(order=3)
-    @pytest.mark.parametrize('a,b,expect', get_datas_div()[0])
+    @pytest.mark.parametrize('a,b,expect', get_datas()["div"]["datas"])
     def test_div(self, get_calc, a, b, expect):
         # result = self.calc.div(a, b)
         result = get_calc.div(a, b)
@@ -108,7 +79,7 @@ class TestCalc:
     # 乘法测试
     # @allure.story("乘法测试")
     @pytest.mark.run(order=5)
-    @pytest.mark.parametrize('a,b,expect', get_datas_mul()[0], ids=get_datas_mul()[1])
+    @pytest.mark.parametrize('a,b,expect', get_datas()["mul"]["datas"], ids=get_datas()["mul"]["ids"])
     def test_mul(self, get_calc, a, b, expect):
         # result = self.calc.add(a, b)
         result = get_calc.mul(a, b)
